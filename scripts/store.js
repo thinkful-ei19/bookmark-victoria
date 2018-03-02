1ab454fae7f5f84e7a02c66007525f3404b29e81
Victoria Daug <victoria.daug@gmail.com>
2018-03-02 20:34:18 +0000

    (feat/refactor) Add filter funtionality
    * refactored item to bookmarks
    * added filter by rating

--- scripts/store.js
+++ scripts/store.js
@@ -2,22 +2,27 @@ const store = (function(){
 
   const addBookmark = function(item) {
     item.expand = false;
-    this.items.push(item);
-    console.log(this.items, 'items')
+    this.bookmarks.push(item);
+    console.log(this.bookmarks, 'bookmarks')
   };
 
   const findById = function(id) {
-    return this.items.find(item => item.id === id);
+    return this.bookmarks.find(item => item.id === id);
   };
 
   const findAndDelete = function(id) {
-    this.items = this.items.filter(item => item.id !== id);
+    this.bookmarks = this.bookmarks.filter(item => item.id !== id);
+  };
+
+  const filterRating = function(setRating) {
+    return this.bookmarks.filter(item => item.rating >= setRating)
   };
 
   return{
-    items: [],
+    bookmarks: [],
     addBookmark: addBookmark,
-    findAndDelete: findAndDelete
+    findAndDelete: findAndDelete,
+    filterRating: filterRating
   };
 })();
 store;
