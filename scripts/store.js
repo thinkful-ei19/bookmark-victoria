2a2d2e80274c8ece1cacc56d39903f94ab5c1ff0
Victoria Daug <victoria.daug@gmail.com>
2018-03-02 16:51:29 +0000

    (feat) Added findById and findAndDelete

--- scripts/store.js
+++ scripts/store.js
@@ -1,10 +1,23 @@
 const store = (function(){
+
   const addItem = function(item) {
+    item.expand = false;
     this.items.push(item);
+    console.log(this.items, 'items')
   };
+
+  const findById = function(id) {
+    return this.items.find(item => item.id === id);
+  };
+
+  const findAndDelete = function(id) {
+    this.items = this.items.filter(item => item.id !== id);
+  };
+
   return{
     items: [],
-    addItem
+    addItem: addItem,
+    findAndDelete: findAndDelete
   };
 })();
 store;
